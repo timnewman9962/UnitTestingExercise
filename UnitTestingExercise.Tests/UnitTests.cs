ed80048741e940827675e3c2afca98b1f1252884
Tim Newman <timnewman9962@gmail.com>
2020-09-14 17:16:06 -0400

    IsPalindrome() tests pass

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,58 +1,95 @@
 using System;
 using Xunit;
+using UnitTestingExercise;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+         [InlineData("hello", false)]
+         [InlineData("123321", true)]
+         [InlineData("45654", true)]
+         [InlineData("racecar", true)]
+
+        public void PalindromeTester(string word, bool expected)
+        {
+            // Arrange
+            var ws = new WordSmith();
+
+            // Action
+            var actual = ws.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(actual, expected);
+        }
+        
+        [Theory]
+        [InlineData(1,2,3,6)]
+        [InlineData(-1,2,3,4)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var funcs = new NumFunctions();
 
             //Act
+            var actual = funcs.Add3Ints(num1, num2, num3);
 
             //Assert
-
+            Assert.Equal(actual, expected);
         }
-
+        
         [Theory]
-        [InlineData()]
+        [InlineData(10, -5, 15)]
+        [InlineData(10, 5, 5)]
+        [InlineData(5, 10, -5)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var funcs = new NumFunctions();
 
             //Act
+            var actual = funcs.Minus(minuend, subtrhend);
 
             //Assert
+            Assert.Equal(actual, expected);
 
         }
-
+        
         [Theory]
-        [InlineData()]
+        [InlineData(10, -5, -50)]
+        [InlineData(10, 5, 50)]
+        [InlineData(10, 100, 1000)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var funcs = new NumFunctions();
 
             //Act
+            var actual = funcs.Mult(num1, num2);
 
             //Assert
+            Assert.Equal(actual, expected);
 
         }
-
+        
         [Theory]
-        [InlineData()]
+        [InlineData(10, -5, -2)]
+        [InlineData(10, 5, 2)]
+        [InlineData(10, 100, 0.1)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var funcs = new NumFunctions();
 
             //Act
+            var actual = funcs.Ratio(num1, num2);
 
             //Assert
+            Assert.Equal(actual, expected);
 
         }
-
+        /*
         [Fact]
         public void YourMethodName()
         {
@@ -73,5 +110,6 @@ namespace UnitTestingExercise.Tests
 
             //Assert
         }
+        */
     }
 }
--- UnitTestingExercise/Program.cs
+++ UnitTestingExercise/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UnitTestingExercise
 {
@@ -7,6 +9,40 @@ namespace UnitTestingExercise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var t1 = word.ToCharArray();
+            Array.Reverse(t1);
+            return word == new string(t1);
+        }
+    }
+
+    public class NumFunctions
+    {
+        public int Add3Ints(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public int Minus(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
+        public int Mult(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Ratio(int dividend, int divisor)
+        {
+            return dividend / divisor;
         }
     }
 }
